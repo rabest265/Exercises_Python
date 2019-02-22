b6f548a92e52b21f16110fdf8c6efdcc76d06727
Rabest265 <rabest265@gmail.com>
2019-02-22 05:05:24 -0500

    Finished PyPoll part of the data

--- python-challenge/PyPoll/main.py
+++ python-challenge/PyPoll/main.py
@@ -1,32 +1,63 @@
-# Unit 3 | Assignment - Py Me Up, Charlie
-
-## PyPoll
-
-#* In this challenge, you are tasked with helping a small, rural town modernize its vote-counting process. 
-# (Up until now, Uncle Cleetus had been trustfully tallying them one-by-one, but unfortunately, his concentration isn't what it used to be.)
-
 #* You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). 
 # The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. 
 # Your task is to create a Python script that analyzes the votes and calculates each of the following:
 
-#  * The total number of votes cast
+import os
+import csv
+
+# Call in the CSV file budget_data.csv and store the output path
+csvpath = os.path.join("..", "Instructions", "election_data.csv")
+output_path = os.path.join("poll_results.txt")
+
+#Set counting variables to 0 to start
+totalvotes = 0
+candidatelist = []
+candidatecount =[]
+largest = 0
+
+#Open the file and read the header in csv reader
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    # Go through each row of data
+    for row in csvreader:
+        candidate =  row[2]
+        # Calculate the total number of votes
+        totalvotes = totalvotes + 1
+        # add value to count if candidate is already there otherwise add them
+        if candidate in candidatelist:
+            candidatecount[candidatelist.index (candidate)] += 1
+        else:
+            candidatelist.append (candidate)
+            candidatecount.append (1)
+
+    
+# Calculate the total number of votes
 #  * A complete list of candidates who received votes
 #  * The percentage of votes each candidate won
 #  * The total number of votes each candidate won
 #  * The winner of the election based on popular vote.
 
-#* As an example, your analysis should look similar to the one below:
 
-#  ```text
-#  Election Results
-#  -------------------------
-#  Total Votes: 3521001
-#  -------------------------
-#  Khan: 63.000% (2218231)
-#  Correy: 20.000% (704200)
-#  Li: 14.000% (492940)
-#  O'Tooley: 3.000% (105630)
-#  -------------------------
-#  Winner: Khan
-#  -------------------------
-#  ```
+#Write an output file with requested information
+    file = open(output_path,"w")
+    file.write("Election Results\n")
+    file.write("----------------------------------------\n")
+    file.write("Total Votes: " + str(totalvotes) + "\n")
+    file.write("----------------------------------------\n")
+    for name in candidatelist:
+        count = candidatecount[candidatelist.index (name)]
+        pct = round(count /totalvotes * 100,3)
+        file.write(name + ":  " + str(pct) + "% (" + str(count) + ")\n")
+        if count > largest:
+            largest = count
+            Winner = name
+    file.write("----------------------------------------\n")
+    file.write("Winner:  " + Winner + "\n")
+    file.write("----------------------------------------")
+    file.close()
+
+#Print the file in terminal
+    file = open(output_path,"r")
+    print (file.read())
+    file.close
